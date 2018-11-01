f93ba9a88b56a6eddaf629d6e2b8e3a380570b3d
mackenziemarques97 <38563099+mackenziemarques97@users.noreply.github.com>
2018-11-01 19:00:46 -0400

    Update Driver_09_21_18/ClassScript_09_21_18.m

--- Driver_09_21_18/ClassScript_09_21_18.m
+++ Driver_09_21_18/ClassScript_09_21_18.m
@@ -1,13 +1,15 @@
-a = ExperimentClass08_29_18('COM8');
+a = ExperimentClass_09_21_18('COM5');
 
-a.linearOscillate(10,10,60,25,30,2,100);
+% a.linearOscillate(10,10,60,25,30,2,100);
+% 
+% a.moveTo(90,45,500);
+% 
+% a.calibrate();
+% 
+% a.smoothPursuit(54000,60,-60,20,1000);
+% 
+% a.calibrate();
 
-a.moveTo(90,45,500);
-
-a.calibrate();
-
-a.smoothPursuit(54000,60,-60,20,1000);
-
-a.calibrate();
+a.moveTo(50,50,100);
 
 a.endSerial();
