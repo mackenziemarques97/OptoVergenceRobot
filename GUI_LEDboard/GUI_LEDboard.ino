d6aa8fd09a57f775298c01051674cfed3ae260ef
mackenziemarques97 <38563099+mackenziemarques97@users.noreply.github.com>
2020-01-14 10:27:30 -0400

    Update GUI_LEDboard.ino

--- GUI_LEDboard/GUI_LEDboard.ino
+++ GUI_LEDboard/GUI_LEDboard.ino
@@ -32,11 +32,12 @@ CRGB leds_Strips[NUM_STRIPS][NUM_LEDS_PER_STRIP]; /* CRGB is an object represent
 CRGB leds_Center[NUM_Center];
 
 /* Define initial variables and arrays */
-int ledNum;
-double dirIndex, colIndex;
+int dir, color, ledNum;
+double timeOn;
+int dirIndex, colIndex;
 String val;
 
-double dirCheck(const char* token) {
+int setDirIndex(const char* token) {
   /* integers in command[1] indicate directions */
   /* case/capitalization matters */
   if (strcmp(token, "N") == 0) { /* if 2nd command entry is N */
@@ -69,7 +70,7 @@ double dirCheck(const char* token) {
   return dirIndex;
 }
 
-double colCheck(const char* token) {
+int setColorIndex(const char* token) {
   /* integers in command[2] indicate LED colors */
   /* case/capitalization matters */
   if (strcmp(token, "red") == 0) { /* if 3rd command entry is red */
@@ -101,19 +102,113 @@ double* parseCommand(char strCommand[]) {
       token = strtok(NULL, delim);
       /*the following if statement assigns numbers to string command entries*/
       if (i == 1) { /* i = 1 indicates 2nd command entry - if looping through and parsing 2nd command entry */
-        command[i] = dirCheck(token);
+        command[i] = setDirIndex(token);
         i++;
       }
       else if (i == 2) { /* i = 2 indicates 3rd command entry - if looping through and parsing 3nd command entry */
-        command[i] = colCheck(token);
+        command[i] = setColorIndex(token);
         i++;
       }
       else { /* for rest of command entries (which should be integers) */
         command[i++] = atof(token); /* put them in command array */
       }
     }
-  
-  return command;
+
+    return command;
+  }
+    if (strcmp(token, "showLEDs") == 0) { /*switch case 1 - oneLED*/
+    static double command[1]; /*5 numerical double command entries are required - oneLED:direction:color:degree offset:time on*/
+    command[0] = 2; /*first number in command array indicates switch case ( 1 = oneLED )*/
+    return command;
+  }
+}
+
+/* function to set the color of the specified LED based on number in command[] */
+void setColor(int dir, int col, int deg) { /* dir, col, deg are all integers stored in command []; re-stored in named variables at start of each switch case */
+  /* if turning on center LED */
+
+  if (dir == 8) {
+    if (col == 1) {
+      leds_Center[0] = CRGB::Red; /* only one LED in leds_Center */
+    }
+    else if (col == 2) {
+      leds_Center[0] = CRGB::Green; /* first and only LED accessed as leds_Center[0] */
+    }
+    else if (col == 3) {
+      leds_Center[0] = CRGB::Blue;
+    }
+  }
+  /* if turning on LEDs in any of the strips */
+  else {
+    if (col == 1) {
+      leds_Strips[dir][deg] = CRGB::Red; /* leds_Strips is an array of arrays */
+    }
+    else if (col == 2) {
+      leds_Strips[dir][deg] = CRGB::Green; /* outer array (dir) refers to each direction strip */
+    }
+    else if (col == 3) {
+      leds_Strips[dir][deg] = CRGB::Blue; /* inner array (deg) refers to one of the 23 LEDs in each direction strip */
+    }
+  }
+}
+
+/* function to scan for valid degree entries and alter all entries to positional numbering scheme */
+int checkDegree(int dir, int deg) {
+  if (dir == 8) { /* leds_Center will only ever have 1 LED in it */
+    if (deg == 0) { /* the only accepted entry for center deg is 0 */
+      //Serial.println("Valid degree entry."); /* since degree offset is measured wrt center LED */
+    }
+    else { /* if any other entry for deg of center LED (dir = 8) */
+      //Serial.println("Invalid degree entry for center LED."); /* invalid entry */
+    }
+  }
+  else { /* if accessing anything except the center LED */
+    if (deg > 20) { /* if outside the section of LEDs in a strip that have 1 degree separation */
+      if (deg == 25) { /* if degree offset from center LED is 25 */
+        ledNum = 20; /* change deg to 20 - that is the position in the strip assigned to LED with 25 degree offset */
+        //Serial.println("Valid degree entry.");
+      }
+      else if (deg == 30) { /* if degree offset is 30 */
+        ledNum = 21; /* change deg to 21 */
+        //Serial.println("Valid degree entry.");
+      }
+      else if (deg == 35) { /*if degree offset is 35 */
+        ledNum = 22; /* change deg to 22 */
+        //Serial.println("Valid degree entry.");
+      }
+      else if (deg > 35) { /* no LEDs beyond a 35 degree offset */
+        ledNum = -1; /* assign arbitrary placeholder deg of -1 */
+        //Serial.println("Error. Inputs exceeds limits."); /* so invalid */
+      }
+      else if ((deg > 20 && deg < 25) || (deg > 25 && deg < 30) || (deg > 30 && deg < 35)) { /* no LEDs between 20 and 25 or 25 and 30 or 30 and 35 degrees */
+        ledNum = -1; /* assign arbitrary placeholder deg of -1 so they won't light up */
+        //Serial.println("Error. Degree entry not an option."); /* so invalid */
+      }
+    }
+    else { /* in any other case */
+      ledNum = deg - 1; /* convert degree offset entry to positional number */
+      //Serial.println("Valid degree entry."); /* example: entry of deg = 1 refers to leds[0], entry of deg = 20 refers to leds[19] */
+    }
+    return ledNum;
+  }
+}
+
+/* simple function to turnOn LED
+   meant to make action of FastLED.show() more transparent
+*/
+void turnOnLED() {
+  FastLED.show(); /* display the color selections set in setColor() */
+}
+
+/* function to turn off LEDs
+  regardless of whether in leds_Center or leds_Strips
+*/
+void turnOff(int dir, int deg) {
+  if (dir == 8) {
+    leds_Center[0] = CRGB::Black; FastLED.show(); /* set LED to black, then display */
+  }
+  else {
+    leds_Strips[dir][deg] = CRGB::Black; FastLED.show();
   }
 }
 
@@ -152,11 +247,22 @@ void loop() {
     double *command = parseCommand(inputArray); /*create pointer variable to parsed commands*/
     /*put objects and variable initializations before start of switch case or block each case so variables remain within each scope*/
     switch ((int) *command) { /*switch case based on first command*/
-      case 1: //sendPhaseParams
+      case 1: //sendPhaseParams:dir:color:degree:timeon
         {
-          for (int i = 1; i < 5; i++) {
-            Serial.println(*(command + i));
-          }
+          dir = * (command + 1);
+          color = * (command + 2);
+          int deg = *(command + 3);
+          timeOn = * (command + 4);
+
+          ledNum = checkDegree(dir, deg);
+          setColor(dir, color, deg);
+        }
+        break;
+      case 2: //showLEDs
+        {
+          turnOnLED();
+          delay(timeOn);
+          turnOff(dir, ledNum);
         }
         break;
     }
